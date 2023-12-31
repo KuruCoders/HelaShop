a0dd402af576ec670f7ad1dae1b4468bf7c94605
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2023-12-31 20:21:15 +0530

    iconsAddedDashBoard

--- frontend/web/admin/src/Components/SideBar/Sidebar.jsx
+++ frontend/web/admin/src/Components/SideBar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import logo from '../../logos/logoAll.svg'
 
 export default function Sidebar() {
     return (
@@ -7,7 +8,7 @@ export default function Sidebar() {
             <div>
                 <div className="brand-logo d-flex align-items-center justify-content-between">
                     <a href="./index.html" className="text-nowrap logo-img">
-                        <img src="../assets/images/logos/dark-logo.svg" width={180}  />
+                        <img src={logo} alt='logo' width={180}  />
                     </a>
                     <div className="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
                         <i className="ti ti-x fs-8" />
@@ -28,24 +29,56 @@ export default function Sidebar() {
                                 <span className="hide-menu">Dashboard</span>
                             </a>
                         </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="./index.html" aria-expanded="false">
+                                <span>
+                                    <i className="ti ti-user" />
+                                </span>
+                                <span className="hide-menu">User Management</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="./index.html" aria-expanded="false">
+                                <span>
+                                    <i className="ti ti-cash" />
+                                </span>
+                                <span className="hide-menu">Payment Management</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="./index.html" aria-expanded="false">
+                                <span>
+                                    <i className="ti ti-building-warehouse" />
+                                </span>
+                                <span className="hide-menu">Inventory Management</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="./index.html" aria-expanded="false">
+                                <span>
+                                    <i className="ti ti-users" />
+                                </span>
+                                <span className="hide-menu">Staff Management</span>
+                            </a>
+                        </li>
                         <li className="nav-small-cap">
                             <i className="ti ti-dots nav-small-cap-icon fs-4" />
                             <span className="hide-menu">AUTH</span>
-                        </li>
-                        <li className="sidebar-item">
-                            <a className="sidebar-link" href="./authentication-login.html" aria-expanded="false">
-                                <span>
-                                    <i className="ti ti-login" />
-                                </span>
-                                <span className="hide-menu">Login</span>
-                            </a>
                         </li>
                         <li className="sidebar-item">
                             <a className="sidebar-link" href="./authentication-register.html" aria-expanded="false">
                                 <span>
                                     <i className="ti ti-user-plus" />
                                 </span>
-                                <span className="hide-menu">Register</span>
+                                <span className="hide-menu">My Profile</span>
+                            </a>
+                        </li>
+                        <li className="sidebar-item">
+                            <a className="sidebar-link" href="./authentication-login.html" aria-expanded="false">
+                                <span>
+                                    <i className="ti ti-login" />
+                                </span>
+                                <span className="hide-menu">Logout</span>
                             </a>
                         </li>
                     </ul>
