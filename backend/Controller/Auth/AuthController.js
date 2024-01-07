6c368378cada00a062e862f1b8ebe01d67f4e20d
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-01-07 18:43:35 +0530

    basicAuthDone

--- backend/Controller/Auth/AuthController.js
+++ backend/Controller/Auth/AuthController.js
@@ -7,6 +7,9 @@ import nodemailer from "nodemailer";
 import registerEmailTemplate from "../../Brokers/Email/RegisterTemplate.js";
 import successEmailTemplate from "../../Brokers/Email/SuccessRegister.js";
 import generateToken from "../../Utils/Auth/Token.js";
+import RegisterEmailSender from '../../Brokers/Email/RegisterEmailSender.js'
+
+
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 class AuthController {
 
@@ -18,37 +21,16 @@ class AuthController {
             if (existing) return response(res, 403, HttpStatus.getStatus(403), ResTypes.errors.user_exists)
             
             const hashedPassword = await bcrypt.hash(password, 10);
-            //send the mail
+           
             const token = Math.random().toString(36).slice(-8)
             const verifyRegisterToken = token
             const registerExpire = Date.now() + 360000
 
-            const user = new User({name,email,password:hashedPassword,role,picUrl,verifyRegisterToken,registerExpire })
-
-            const transporter = nodemailer.createTransport({
-                service: process.env.MAIL_SERVER,
-                auth: {
-                    user: process.env.MAIL_USERNAME,
-                    pass: process.env.MAIL_PASSWORD,
-                }
-            })
-            const msg = {
-                from: "qwwerrrty11@gmail.com",
-                to: email,
-                subject: "Email verification",
-                html: registerEmailTemplate(token),
-            }
-            transporter.sendMail(msg, async(err, info) => {
-                if (err) {
-                    console.log(err)
-                    return response(res, 404, HttpStatus.getStatus(404), { message: err })
-                }
-                //save the user
-                await user.save();
-                return response(res,200,HttpStatus.getStatus(200),{message:"Email Sent to "+email})
-            })
+            const user = new User({ name, email, password: hashedPassword, role, picUrl, verifyRegisterToken, registerExpire })
+             //send the mail
+            RegisterEmailSender.sendVerificationEmail(user, token, res)
         } catch (error) {
-            return response(res,500,HttpStatus.getStatus(500))
+            return response(res,500,HttpStatus.getStatus(500),{message:error})
         }
     }
     //email verify endpoint
