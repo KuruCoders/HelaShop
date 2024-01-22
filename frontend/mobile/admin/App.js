3b0597a26a1037872ce9e05ff5215429fd71452a
Mohammad Shabeer <103739510+moshdev2213@users.noreply.github.com>
2024-01-22 16:55:00 +0530

    resoveNativeVersionErr

--- frontend/mobile/admin/App.js
+++ frontend/mobile/admin/App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from "expo-font";
+import Welcome from "./screens/Welcome";
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    "Montserrat-Black": require("./assets/fonts/Montserrat-Black.ttf"),
+    "Montserrat-Italic": require("./assets/fonts/Montserrat-Italic.ttf"),
+    "Montserrat-BlackItalic": require("./assets/fonts/Montserrat-BlackItalic.ttf"),
+    "Montserrat-Bold": require("./assets/fonts/Montserrat-Bold.ttf"),
+    "Montserrat-BoldItalic": require("./assets/fonts/Montserrat-BoldItalic.ttf"),
+    "Montserrat-ExtraBold": require("./assets/fonts/Montserrat-ExtraBold.ttf"),
+    "Montserrat-ExtraBoldItalic": require("./assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
+    "Montserrat-ExtraLight": require("./assets/fonts/Montserrat-ExtraLight.ttf"),
+    "Montserrat-ExtraLightItalic": require("./assets/fonts/Montserrat-ExtraLightItalic.ttf"),
+    "Montserrat-Light": require("./assets/fonts/Montserrat-Light.ttf"),
+    "Montserrat-LightItalic": require("./assets/fonts/Montserrat-LightItalic.ttf"),
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-MediumItalic": require("./assets/fonts/Montserrat-MediumItalic.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+    "Montserrat-SemiBold": require("./assets/fonts/Montserrat-SemiBold.ttf"),
+    "Montserrat-SemiBoldItalic": require("./assets/fonts/Montserrat-SemiBoldItalic.ttf"),
+    "Montserrat-Thin": require("./assets/fonts/Montserrat-Thin.ttf"),
+    "Montserrat-ThinItalic": require("./assets/fonts/Montserrat-ThinItalic.ttf"),
+  });
+
+  if (!loaded) {
+    return error;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Welcome/>
+      {/* <Login /> */}
+      {/* <Register/> */}
+      {/* <HomeScreen /> */}
+      {/* <Splash/> */}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- frontend/mobile/admin/tailwind.config.js
+++ frontend/mobile/admin/tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}","./navigation/**/*.{js,jsx,ts,tsx}","./components/**/*.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./screens/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ["Montserrat-Regular"],
+        montBold: ["Montserrat-Bold"],
+        montBlack: ["Montserrat-Black"],
+        montItalic: ["Montserrat-Italic"],
+        montBlackItalic: ["Montserrat-BlackItalic"],
+        montBold: ["Montserrat-Bold"],
+        montBoldItalic: ["Montserrat-BoldItalic"],
+        montExtraBold: ["Montserrat-ExtraBold"],
+        montExtraBoldItalic: ["Montserrat-ExtraBoldItalic"],
+        montExtraLight: ["Montserrat-ExtraLight"],
+        montExtraLightItalic: ["Montserrat-ExtraLightItalic"],
+        montLight: ["Montserrat-Light"],
+        montLightItalic: ["Montserrat-LightItalic"],
+        montMedium: ["Montserrat-Medium"],
+        montMediumItalic: ["Montserrat-MediumItalic"],
+        montSemiBold: ["Montserrat-SemiBold"],
+        montSemiBoldItalic: ["Montserrat-SemiBoldItalic"],
+        montThin: ["Montserrat-Thin"],
+        montThinItalic: ["Montserrat-ThinItalic"],
+      },      
+    },
   },
   plugins: [],
 }
